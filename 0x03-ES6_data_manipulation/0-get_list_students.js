4ffbd27d9f9500256786c71f7c75eb5be76d91d1
Elocodes <elochukwuodoeze@gmail.com>
2023-12-04 20:32:43 +0000

    getlist fun

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+// function returns an array of objects
+export default function getListStudents() {
+    return [
+	    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+	    { id: 2, firstName: 'James', location: 'Columbia' },
+	    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    ]
+}
